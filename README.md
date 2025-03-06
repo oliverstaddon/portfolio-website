7521cde75f63bad0c9ce019fc12a8598bc54a3ff
Oliver Staddon <118471380+oliverstaddon@users.noreply.github.com>
2025-03-06 19:43:55 +0000

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# oliverstaddon.github.io
+# My portfolio
+This is a simple portfolio website designed as part of a university module.
---
+++ assets/scripts/stars.js
@@ -0,0 +1,19 @@
+
+window.onload = function(){
+    // num stars and array to hold
+    const numStars = 150;
+
+    for(let i = 0; i < numStars; i++){
+        // create star div and assign css class
+        const star = document.createElement('div');
+        star.className = 'star';
+
+        // get random width and height(whole page) position 
+        star.style.left = `${Math.random() * 99}vw`;
+        star.style.top = `${Math.random() * document.documentElement.scrollHeight}px`;
+
+        // assign random animation delay
+        star.style.animationDelay = `${Math.random() * 5}s`;
+        document.body.appendChild(star);
+    }
+};
